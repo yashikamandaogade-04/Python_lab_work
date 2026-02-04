4fe3fded0f54c80e9d11e8a03156eeeeca348766
yashika <yashikamandaogade@gmail.com>
2026-02-04 15:01:47 +0530

    1 practical

--- 1.py
+++ 1.py
@@ -1,13 +1,21 @@
-import numpy as np
-a = []
-b = []
-for i in range(5):
-    row = list(map(int, input().split()))
-    a.append(row)
-for i in range(3):
-    row = list(map(int, input().split()))
-    b.append(row)
-a = np.array(a)
-b = np.array(b)
-c = a @ b
-print(c)
+#employee details
+name = input("Enter Employee Name: ")
+emp_id = int(input("Enter Employee ID: "))
+department = input("Enter Department: ")
+basic_salary = float(input("Enter Basic Salary: "))
+#formula to calculate salary
+da = 0.92 * basic_salary
+hra = 0.58 * basic_salary
+ta = 0.30 * basic_salary
+gross_salary = basic_salary + da + hra + ta
+net_salary = gross_salary - 500
+print("\n--- Employee Salary Details ---")
+print("Name:", name)
+print("Employee ID:", emp_id)
+print("Department:", department)
+print("Basic Salary:", basic_salary)
+print("DA (92%):", da)
+print("HRA (58%):", hra)
+print("TA (30%):", ta)
+print("Gross Salary:", gross_salary)
+print("Net Salary (after LIC deduction):", net_salary)
